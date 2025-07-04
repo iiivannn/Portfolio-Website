330715df63b50f7ffb5cb717ac5e2831ec1b098f
ivan <oraionstarr@gmail.com>
2025-07-04 23:27:26 +0800

    home page animation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
+
+import ScrollReveal from "scrollreveal";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
@@ -33,6 +35,10 @@ export default function Home() {
   const heroText = useRef(null);
   const heroImg = useRef(null);
   const contentCard = useRef(null);
+  const projectsRef = useRef(null);
+  const certificatesRef = useRef(null);
+  const skillsRef = useRef(null);
+  const contactRef = useRef(null);
 
   useEffect(() => {
     const goTop = () => {
@@ -56,50 +62,293 @@ export default function Home() {
   };
 
   useEffect(() => {
-    if (heroText.current && heroImg.current) {
+    if (heroText.current) {
+      ScrollReveal().reveal(heroText.current.children, {
+        origin: "left",
+        distance: "50px",
+        opacity: 0,
+        duration: 1000,
+        ease: "power2.out",
+        interval: 250,
+        reset: true,
+        delay: 500,
+      });
+    }
+    if (heroImg.current) {
+      ScrollReveal().reveal(heroImg.current, {
+        origin: "right",
+        distance: "50px",
+        opacity: 0,
+        duration: 1000,
+        ease: "power2.out",
+        delay: 500,
+        reset: true,
+      });
+    }
+
+    if (contentCard.current) {
+      ScrollReveal().reveal(contentCard.current.children, {
+        origin: "bottom",
+        distance: "50px",
+        opacity: 0,
+        duration: 1000,
+        ease: "power2.out",
+        interval: 1000,
+        reset: true,
+        delay: 250,
+      });
+    }
+
+    if (projectsRef.current) {
       gsap.fromTo(
-        heroText.current.children,
-        { opacity: 0, x: -100 },
+        projectsRef.current.querySelector(".project-text").children,
+        {
+          opacity: 0,
+          y: 50,
+        },
         {
           opacity: 1,
-          x: 0,
-          duration: 2,
-          ease: "power2.out",
-          stagger: 0.1,
-        }
-      );
-      gsap.fromTo(
-        heroImg.current.children,
-        { opacity: 0, x: 100 },
-        {
-          opacity: 1,
-          x: 0,
+          y: 0,
           duration: 1,
           ease: "power2.out",
           stagger: 0.2,
+          scrollTrigger: {
+            trigger: projectsRef.current.querySelector(".project-text"),
+            start: "top 90%",
+            end: "bottom 20%",
+            toggleActions: "play none none reset",
+          },
+        }
+      );
+
+      const projectItems =
+        projectsRef.current.querySelectorAll(".project-item");
+      projectItems.forEach((item, index) => {
+        // Main container animation
+        gsap.fromTo(
+          item,
+          {
+            y: 80,
+            opacity: 0,
+            scale: 0.8,
+          },
+          {
+            y: 0,
+            opacity: 1,
+            scale: 1,
+            duration: 1,
+            ease: "power2.out",
+            scrollTrigger: {
+              trigger: item,
+              start: "top 85%",
+              end: "bottom 15%",
+              toggleActions: "play none none reverse",
+            },
+            delay: index * 0.2,
+          }
+        );
+      });
+
+      const projectTitle = projectsRef.current.querySelectorAll(".item-title");
+      projectTitle.forEach((title) => {
+        gsap.fromTo(
+          title.children,
+          {
+            opacity: 0,
+            y: 50,
+          },
+          {
+            y: 0,
+            opacity: 1,
+            duration: 1,
+            ease: "power2.out",
+            stagger: 0.2,
+
+            scrollTrigger: {
+              trigger: title,
+              start: "top 90%",
+              end: "bottom 20%",
+              toggleActions: "play none none reset",
+            },
+            delay: 0.2,
+          }
+        );
+      });
+    }
+
+    if (certificatesRef.current) {
+      ScrollReveal().reveal(certificatesRef.current.querySelector("h2"), {
+        origin: "top",
+        distance: "30px",
+        opacity: 0,
+        duration: 800,
+        easing: "ease-out",
+        reset: true,
+      });
+
+      ScrollReveal().reveal(
+        certificatesRef.current.querySelectorAll(".cert-item"),
+        {
+          origin: "bottom",
+          distance: "40px",
+          opacity: 0,
+          duration: 1000,
+          easing: "ease-out",
+          interval: 300,
+          reset: true,
+          delay: 200,
         }
       );
     }
 
-    if (contentCard.current) {
-      gsap.fromTo(
-        contentCard.current.children,
-        { opacity: 0, x: 200 },
-        {
-          opacity: 1,
-          x: 0,
-          duration: 1,
-          stagger: 0.2,
-          ease: "power2.out",
-          scrollTrigger: {
-            trigger: ".content-card",
-            start: "top 70%",
-            toggleActions: "play pause resume reverse",
+    if (skillsRef.current) {
+      const techSkillBox = skillsRef.current.querySelector(".tech-skill-box");
+      if (techSkillBox) {
+        gsap.fromTo(
+          techSkillBox.querySelector(".skill-list"),
+          {
+            x: -100,
+            opacity: 0,
           },
-        }
-      );
+          {
+            x: 0,
+            opacity: 1,
+            duration: 1,
+            scrollTrigger: {
+              trigger: techSkillBox,
+              start: "top 80%",
+              end: "bottom 20%",
+              toggleActions: "play none none reverse",
+            },
+          }
+        );
+
+        gsap.fromTo(
+          techSkillBox.querySelector(".skill-img"),
+          {
+            x: 100,
+            opacity: 0,
+          },
+          {
+            x: 0,
+            opacity: 1,
+            duration: 1,
+            scrollTrigger: {
+              trigger: techSkillBox,
+              start: "top 80%",
+              end: "bottom 20%",
+              toggleActions: "play none none reverse",
+            },
+            delay: 0.2,
+          }
+        );
+
+        const skillItems = techSkillBox.querySelectorAll(".skill-items p");
+        skillItems.forEach((item, index) => {
+          gsap.fromTo(
+            item,
+            {
+              y: 20,
+              opacity: 0,
+            },
+            {
+              y: 0,
+              opacity: 1,
+              duration: 0.5,
+              scrollTrigger: {
+                trigger: item,
+                start: "top 90%",
+                end: "bottom 10%",
+                toggleActions: "play none none reverse",
+              },
+              delay: index * 0.1,
+            }
+          );
+        });
+      }
+
+      const softSkillBox = skillsRef.current.querySelector(".soft-skill-box");
+      if (softSkillBox) {
+        gsap.fromTo(
+          softSkillBox.querySelector(".skill-img"),
+          {
+            x: -100,
+            opacity: 0,
+          },
+          {
+            x: 0,
+            opacity: 1,
+            duration: 1,
+            scrollTrigger: {
+              trigger: softSkillBox,
+              start: "top 80%",
+              end: "bottom 20%",
+              toggleActions: "play none none reverse",
+            },
+          }
+        );
+
+        gsap.fromTo(
+          softSkillBox.querySelector(".soft-skill-list"),
+          {
+            x: 100,
+            opacity: 0,
+          },
+          {
+            x: 0,
+            opacity: 1,
+            duration: 1,
+            scrollTrigger: {
+              trigger: softSkillBox,
+              start: "top 80%",
+              end: "bottom 20%",
+              toggleActions: "play none none reverse",
+            },
+            delay: 0.2,
+          }
+        );
+
+        const softSkillItems = softSkillBox.querySelectorAll(".check-item");
+        softSkillItems.forEach((item, index) => {
+          gsap.fromTo(
+            item,
+            {
+              y: 20,
+              opacity: 0,
+            },
+            {
+              y: 0,
+              opacity: 1,
+              duration: 0.6,
+              scrollTrigger: {
+                trigger: item,
+                start: "top 90%",
+                end: "bottom 10%",
+                toggleActions: "play none none reverse",
+              },
+              delay: index * 0.1,
+            }
+          );
+        });
+      }
     }
+    if (contactRef.current) {
+      ScrollReveal().reveal(contactRef.current, {
+        origin: "bottom",
+        opacity: 0.8,
+        duration: 500,
+        easing: "ease-in",
+        reset: true,
+      });
+    }
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => {
+        trigger.kill;
+      });
+    };
   }, []);
+
   return (
     <div className="home-page">
       <Navbar />
@@ -126,7 +375,12 @@ export default function Home() {
           </div>
           <div ref={heroImg} className="hero-img">
             <div className="pic_box"></div>
-            <img className="hero_pic" src={hero_pic} alt="Hero Image" />
+            <img
+              className="hero_pic"
+              loading="lazy"
+              src={hero_pic}
+              alt="Hero Image"
+            />
           </div>
         </div>
       </section>
@@ -144,6 +398,7 @@ export default function Home() {
           <div ref={contentCard} className="content-card">
             <div className="card-item">
               <img
+                loading="lazy"
                 className="content-dev"
                 src={content_dev}
                 alt="Developer Offer"
@@ -156,6 +411,7 @@ export default function Home() {
             </div>
             <div className="card-item">
               <img
+                loading="lazy"
                 className="content-front"
                 src={content_front}
                 alt="Frontend Offer"
@@ -167,6 +423,7 @@ export default function Home() {
             </div>
             <div className="card-item">
               <img
+                loading="lazy"
                 className="content-back"
                 src={content_back}
                 alt="Backend Offer"
@@ -178,7 +435,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="home-projects">
+      <section className="home-projects" ref={projectsRef}>
         <div className="project-text">
           <p className="sub-title">Projects</p>
           <div className="project-title">
@@ -187,104 +444,113 @@ export default function Home() {
         </div>
         <div className="project-box">
           <div className="project-item">
-            <div className="item-title">
-              <h2>ParSafe</h2>
-              <p>
-                The user interface of an automated parcel receiver named
-                ParSafe. Managing parcel information and status are done in the
-                website.
-              </p>
-            </div>
             <a href="https://supabase-users.vercel.app/" target="_blank">
-              <div className="proj-pic-wrapper">
+              <div className="item-title">
+                <h2>ParSafe</h2>
+                <p>
+                  The user interface of an automated parcel receiver named
+                  ParSafe. Managing parcel information and status are done in
+                  the website.
+                </p>
+              </div>
+              <div className="proj-pic-wrapper tooltip-container">
                 <img
+                  loading="lazy"
                   className="proj-pic"
                   src={project1}
                   alt="Project 1 Sample Image"
                 />
+                <span className="tooltip">Visit Website</span>
               </div>
             </a>
           </div>
+
           <div className="project-item item-2">
-            <div className="item-title">
-              <h2>Urban Grandeur</h2>
-              <p>
-                A landing page for a mockup architectural company. Showcasing
-                the company&apos;s info using React.
-              </p>
-            </div>
             <a
               href="https://architectural-website-two.vercel.app/"
               target="_blank"
             >
-              <div className="proj-pic-wrapper">
+              <div className="item-title">
+                <h2>Urban Grandeur</h2>
+                <p>
+                  A landing page for a mockup architectural company. Showcasing
+                  the company&apos;s info using React.
+                </p>
+              </div>
+              <div className="proj-pic-wrapper tooltip-container">
                 <img
+                  loading="lazy"
                   className="proj-pic"
                   src={project2}
                   alt="Project 2 Sample Image"
                 />
+                <span className="tooltip">Visit Website</span>
               </div>
             </a>
           </div>
           <div className="project-item item-3">
-            <div className="item-title">
-              <h2>UrCoffee</h2>
-              <p>
-                An e-commerce website for a coffee shop. It features the
-                products as well as the information about the shop.
-              </p>
-            </div>
             <a
               href="https://coffee-website-seven-indol.vercel.app/"
               target="_blank"
             >
-              <div className="proj-pic-wrapper">
+              <div className="item-title">
+                <h2>UrCoffee</h2>
+                <p>
+                  An e-commerce website for a coffee shop. It features the
+                  products as well as the information about the shop.
+                </p>
+              </div>
+              <div className="proj-pic-wrapper tooltip-container">
                 <img
+                  loading="lazy"
                   className="proj-pic"
                   src={project3}
                   alt="Project 3 Sample Image"
                 />
+                <span className="tooltip">Visit Website</span>
               </div>
             </a>
           </div>
           <div className="project-item item-4">
-            <div className="item-title">
-              <h2>Fall Auto</h2>
-              <p>
-                A website for car dealership. It showcases various car brands as
-                well as their prices, special offers, and model.
-              </p>
-            </div>
             <a href="https://car-website-wheat.vercel.app/" target="_blank">
-              <div className="proj-pic-wrapper">
+              <div className="item-title">
+                <h2>Fall Auto</h2>
+                <p>
+                  A website for car dealership. It showcases various car brands
+                  as well as their prices, special offers, and model.
+                </p>
+              </div>
+              <div className="proj-pic-wrapper tooltip-container">
                 <img
+                  loading="lazy"
                   className="proj-pic"
                   src={project4}
                   alt="Project 4 Sample Image"
                 />
+                <span className="tooltip">Visit Website</span>
               </div>
             </a>
           </div>
         </div>
       </section>
 
-      <section className="certificates">
+      <section ref={certificatesRef} className="certificates">
         <div className="cert-box">
           <h2>Certifications</h2>
           <div className="certs">
             <div className="cert-item ">
               <p>CS50</p>
-              <img src={cs50} alt="cs50 logo" />
+              <img loading="lazy" src={cs50} alt="cs50 logo" />
             </div>
             <div className="cert-item ">
               <p>freeCodeCamp</p>
-              <img src={fcc} alt="fcc logo" />
+              <img loading="lazy" src={fcc} alt="fcc logo" />
             </div>
           </div>
         </div>
       </section>
 
-      <section className="skills">
+      <section ref={skillsRef} className="skills">
         <div className="skill-box">
           <div className="tech-skill-box">
             <div className="tech-skills">
@@ -307,11 +573,13 @@ export default function Home() {
               </div>
               <div className="skill-img">
                 <img
+                  loading="lazy"
                   className="skill-img-back"
                   src={tech}
                   alt="Technology Background"
                 />
                 <img
+                  loading="lazy"
                   className="skill-img-front"
                   src={tech}
                   alt="Laptop with Code"
@@ -324,11 +592,13 @@ export default function Home() {
             <div className="soft-skill-box">
               <div className="skill-img">
                 <img
+                  loading="lazy"
                   className="skill-img-back"
                   src={soft}
                   alt="Team Meeting Background"
                 />
                 <img
+                  loading="lazy"
                   className="skill-img-front"
                   src={soft}
                   alt="Team Collaboration"
@@ -368,7 +638,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="contact">
+      <section ref={contactRef} id="contact">
         <Footer />
       </section>
 
